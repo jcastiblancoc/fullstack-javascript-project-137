032de9178e5f4afae025abec8145bb4f151fc762
jcastiblanco <castiblanco.jeisson@gmail.com>
2025-09-14 17:28:24 -0500

    fix errors

--- src/validation.js
+++ src/validation.js
@@ -1,17 +1,5 @@
 import * as yup from 'yup';
 
-export const configureYup = (i18n) => {
-  yup.setLocale({
-    mixed: {
-      required: i18n.t('errors.required'),
-      notOneOf: i18n.t('errors.notOneOf'),
-    },
-    string: {
-      url: i18n.t('errors.url'),
-    },
-  });
-};
-
 export const buildSchema = (existingUrls) => (
   yup.string()
     .required()
