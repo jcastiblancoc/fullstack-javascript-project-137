49d266823a77d575f822e9144ee820db1aec63fb
jcastiblanco <castiblanco.jeisson@gmail.com>
2025-09-14 18:24:19 -0500

    fix errors

--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from '@playwright/test';
-
-export default defineConfig({
-  testDir: './tests',
-  use: {
-    headless: true,
-  },
-});
--- tests/example.spec.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('dummy test - page loads', async ({ page }) => {
-  await page.goto('https://example.com');
-  await expect(page).toHaveTitle(/Example/);
-});
